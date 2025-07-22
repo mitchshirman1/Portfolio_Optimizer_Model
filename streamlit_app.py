5e889fb3ff933cd18f53d4f60f10bd9725dc8f5d
mitchshirman1 <mitch.shirman@yahoo.com>
2025-07-22 17:13:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import random
 
+st.set_option("client.showWarningOnDirectSessionStateAccess", False)
+
 st.markdown("""
 <style>
 /* Sidebar background */
@@ -135,9 +137,8 @@ with tab1:
     tickers_input = st.sidebar.text_input("Enter stock tickers separated by commas: ", key="tickers_input")
     tickers = [t.strip().upper() for t in tickers_input.split(",") if t.strip() != ""]
 
-    if "end_date" not in st.session_state:
-        st.session_state["end_date"] = datetime.today()
-    end_date = st.sidebar.date_input("Select end date", key="end_date")
+    default_end_date = st.session_state.get("end_date", datetime.today())
+    end_date = st.sidebar.date_input("Select end date", value=default_end_date, key="end_date")
     start_date = end_date.replace(year=end_date.year - 3)
 
     total_investment = st.sidebar.number_input("Total investment amount ($)", min_value=100.00, value=st.session_state.get("investment_amount", 1000.0), key="investment_amount")
